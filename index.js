123e9b8f3d529812b46571ad12f0aeb6ca3e1d32
Vinicius dos Santos Verissimo <viniciusdsv93@gmail.com>
2024-03-18 07:08:10 -0300

    feat: create routes configuration file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const router = require("./routes/routes");
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-	res.status(200).send(`Hello world: service ${process.env.NAME}`);
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
